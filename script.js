ed796a5e97fa662831a7380876d406d106ca7349
KwabenaNkrumahRichard <nkrumahrichard62@gmail.com>
2024-11-22 17:34:02 +0000

    Added html and css

---
+++ script.js
@@ -0,0 +1,16 @@
+// console.log("We".length / 3);
+
+// let splitAddPlus = (words) => words.split(" ").join("+");
+
+// console.log(splitAddPlus("nationwide health insurance"));
+
+// let apiKey =
+//   "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=json";
+
+// fetch(
+//   `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ghana?unitGroup=metric&key=UD7D65FGYYPBC57V5MB9TR687&contentType=json`
+// )
+//   .then((respond) => respond.json())
+//   .then((data) => {
+//     console.log(data.days[0]);
+//   });
