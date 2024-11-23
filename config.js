6046b96fba16fec185b5f7c0fefa420d715cdf36
KwabenaNkrumahRichard <nkrumahrichard62@gmail.com>
2024-11-23 17:03:01 +0000

    added config js and script js

---
+++ config.js
@@ -0,0 +1,128 @@
+let weatherContainer = document.querySelector(".weather");
+
+const renderSpinner = function (element) {
+  const marker = `<div class="loader"></div>;`;
+
+  element.textContent = "";
+  element.innerHTML = marker;
+};
+
+let createContainers = function (classValue) {
+  let div = document.createElement("div");
+  div.classList.add(classValue);
+  weatherContainer.append(div);
+};
+
+let wholeNumber = function (value) {
+  return Math.trunc(value);
+};
+
+let getCondition = function (condition) {
+  return [condition.split(",")[0]];
+};
+
+const renderTodaysWeatherUI = function (value) {
+  createContainers("todays-weather");
+  let div = document.querySelector(".todays-weather");
+
+  let todayHTML = `<h2 class="todays-weather-heading">Today</h2>
+                        <div class="todays-weather-temp">
+                          <div class="todays-current-temp">${wholeNumber(
+                            value.temp
+                          )}℃</div>
+                          <div class="todays-current-icon">
+                            <img src="icons/${value.icon}.svg" alt="" />
+                          </div>
+                        </div>
+                        <p class="todays-weather-condition">${getCondition(
+                          value.conditions
+                        )}</p>
+                        <p class="todays-weather-tempminmax">
+                          <span class="min-temp">${wholeNumber(
+                            value.tempmin
+                          )}℃</span>
+                          <span class="max-temp">${wholeNumber(
+                            value.tempmax
+                          )}℃</span>
+                        </p>`;
+
+  div.insertAdjacentHTML("afterbegin", todayHTML);
+};
+
+const renderTodaysHourlyWeatherUI = function (hours, day) {
+  let dayDate = day.datetime;
+  let currentHour = new Date().getHours();
+
+  createContainers("todays-weather-hourly");
+  let div = document.querySelector(".todays-weather-hourly");
+  hours.forEach((hour) => {
+    let dayHour = new Date(`${dayDate},${hour.datetime}`).getHours();
+    let todayHourlyHTML = `<div class="hourly">
+                              <p class="hourly-time">${
+                                currentHour === dayHour
+                                  ? "Now"
+                                  : hour.datetime.slice(0, 5)
+                              }</p>
+                              <div class="hourly-icon">
+                                <img src="icons/${hour.icon}.svg" alt="" />
+                              </div>
+                              <p class="hourly-temp">${wholeNumber(
+                                hour.temp
+                              )}℃</p>
+                            </div>`;
+
+    div.insertAdjacentHTML("beforeend", todayHourlyHTML);
+  });
+};
+
+let displayDay = function (value) {
+  if (value === 0) return "Sunday";
+  if (value === 1) return "Monday";
+  if (value === 2) return "Tuesday";
+  if (value === 3) return "Wednesday";
+  if (value === 4) return "Thursday";
+  if (value === 5) return "Friday";
+  if (value === 6) return "Saturday";
+};
+
+const renderNextSevendaysUI = function (days) {
+  // let div = document.createElement("div");
+  // div.classList.add("next-sevendays-weather");
+  // weatherContainer.append(div);
+  createContainers("next-sevendays-weather");
+  let div = document.querySelector(".next-sevendays-weather");
+
+  let para = document.createElement("p");
+  para.classList.add("text");
+  para.textContent = "7-DAY-FORCAST";
+  div.append(para);
+
+  days.forEach((day) => {
+    let dayNum = new Date(day.datetime).getDay();
+    let nextSevenDaysHTML = `<div class="day">
+                              <p class="day-name">${displayDay(dayNum)}</p>
+                              <div class="day-icon">
+                                <img src="icons/${day.icon}.svg" alt="" />
+                              </div>
+                              <p class="day-min-temp">${wholeNumber(
+                                day.tempmin
+                              )}℃</p>
+                              <p class="day-max-temp">${wholeNumber(
+                                day.tempmax
+                              )}℃</p>
+                              <p class="day-condition">${getCondition(
+                                day.conditions
+                              )}</p>
+                            </div>`;
+
+    div.insertAdjacentHTML("beforeend", nextSevenDaysHTML);
+  });
+};
+
+export {
+  renderSpinner,
+  renderTodaysWeatherUI,
+  weatherContainer,
+  renderTodaysHourlyWeatherUI,
+  renderNextSevendaysUI,
+};
--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-// console.log("We".length / 3);
+import {
+  renderSpinner,
+  renderTodaysWeatherUI,
+  weatherContainer,
+  renderTodaysHourlyWeatherUI,
+  renderNextSevendaysUI,
+} from "./config.js";
 
-// let splitAddPlus = (words) => words.split(" ").join("+");
+const form = document.querySelector("form");
+const countryValue = document.querySelector('input[type="text"]');
 
-// console.log(splitAddPlus("nationwide health insurance"));
+const fetchAPI = function (country) {
+  renderSpinner(weatherContainer);
 
-// let apiKey =
-//   "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=json";
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${country}?unitGroup=metric&key=UD7D65FGYYPBC57V5MB9TR687&contentType=json`
+  )
+    .then((respond) => {
+      if (!respond.ok)
+        throw new Error(`Could not find Country ${respond.status}`);
+      else return respond.json();
+    })
+    .then((data) => {
+      weatherContainer.textContent = "";
 
-// fetch(
-//   `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ghana?unitGroup=metric&key=UD7D65FGYYPBC57V5MB9TR687&contentType=json`
-// )
-//   .then((respond) => respond.json())
-//   .then((data) => {
-//     console.log(data.days[0]);
-//   });
+      const [today] = data.days;
+      const nextSevenDays = data.days.slice(1, 8);
+      renderTodaysWeatherUI(today);
+
+      renderTodaysHourlyWeatherUI(today.hours, today);
+
+      renderNextSevendaysUI(nextSevenDays);
+      console.log(data);
+    })
+    .catch((err) => {
+      weatherContainer.textContent = "";
+      console.log(err.message);
+      weatherContainer.innerHTML = `<p class="error-message">
+                                      Could not find data about country.....<br> TRY AGAIN
+                                    </p>`;
+    });
+};
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let country = countryValue.value.toLowerCase();
+  countryValue.value = "";
+  countryValue.blur();
+
+  fetchAPI(country);
+});
